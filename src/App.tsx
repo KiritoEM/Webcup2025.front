b2e7050bd2067afc420db6a5a4f4689b8fd6edea
RAKOTONDRAZAKA Nameno Fanantenana <mrnrakotondrazaka@gmail.com>
2025-05-17 18:55:05 +0300

    feat(loader): add loader animation

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import { JSX } from "react";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./lib/i18n.ts";
 import Cursor from "./components/cursor/index.tsx";
+import Loader from "./components/loader/index.tsx";
 
 const App = (): JSX.Element => {
     return (
         <I18nextProvider i18n={i18n}>
             <ThemeProvider>
+                <Loader />
                 <Cursor />
                 <RouterProvider router={router} />
             </ThemeProvider>
