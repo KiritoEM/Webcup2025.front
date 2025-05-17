808d898bc9f244ce16bc2c8b5f0f7e8cb2754d51
RAKOTONDRAZAKA Nameno Fanantenana <mrnrakotondrazaka@gmail.com>
2025-05-17 23:04:55 +0300

    feat(auth): integration backend

--- src/lib/form-validation/index.ts
+++ src/lib/form-validation/index.ts
@@ -2,26 +2,20 @@ import { z } from "zod";
 
 const loginSchema = z.strictObject({
     email: z.string().email({ message: "L'email est requis !" }),
-    password: z
-        .string()
-        .min(8, {
-            message: "Le mot de passe doit contenir au moins 8 caractères !",
-        }),
+    password: z.string().min(8, {
+        message: "Le mot de passe doit contenir au moins 8 caractères !",
+    }),
 });
 
 const signupSchema = z.strictObject({
     username: z.string().min(1, { message: "Le nom complet est requis !" }),
     email: z.string().email({ message: "L'email est requis !" }),
-    password1: z
-        .string()
-        .min(8, {
-            message: "Le mot de passe doit contenir au moins 8 caractères !",
-        }),
-    password2: z
-        .string()
-        .min(8, {
-            message: "Le mot de passe doit contenir au moins 8 caractères !",
-        }),
+    password1: z.string().min(8, {
+        message: "Le mot de passe doit contenir au moins 8 caractères !",
+    }),
+    password2: z.string().min(8, {
+        message: "Le mot de passe doit contenir au moins 8 caractères !",
+    }),
 });
 
 export type LoginSchemaTypes = z.infer<typeof loginSchema>;
