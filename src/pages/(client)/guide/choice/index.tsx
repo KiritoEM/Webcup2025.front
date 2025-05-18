926b92e25de2cbcfae20c57971833e1316c7dbea
RAKOTONDRAZAKA Nameno Fanantenana <mrnrakotondrazaka@gmail.com>
2025-05-18 05:47:50 +0300

    feat(guide): add linking

--- src/pages/(client)/guide/choice/index.tsx
+++ src/pages/(client)/guide/choice/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { JSX } from "react";
+import { Link } from "react-router-dom";
 
 const Choice = (): JSX.Element => {
     return (
@@ -14,12 +15,16 @@ const Choice = (): JSX.Element => {
                 </p>
             </div>
             <div className="mt-4 flex flex-col gap-4">
-                <Button className="btn rounded-2xl bg-black hover:bg-black dark:bg-white">
-                    Me laisser guide
-                </Button>
-                <Button className="btn rounded-2xl bg-black hover:bg-black dark:bg-white">
-                    Je connais ce que je ressens
-                </Button>
+                <Link to={"/guide/emotions"} className="w-full">
+                    <Button className="btn w-full rounded-2xl bg-black hover:bg-black dark:bg-white">
+                        Me laisser guide
+                    </Button>
+                </Link>
+                <Link to={"/guide/chat"} className="w-full">
+                    <Button className="btn w-full rounded-2xl bg-black hover:bg-black dark:bg-white">
+                        Je connais ce que je ressens
+                    </Button>
+                </Link>
             </div>
         </section>
     );
